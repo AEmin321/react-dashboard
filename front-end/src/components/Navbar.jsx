a7eeee3296af7a52879927d1ad2d341c7100d73a
hamini <ahmademin321@gmail.com>
2024-03-22 20:40:19 +0300

    Add Navbar component and layout setup

---
+++ front-end/src/components/Navbar.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import {
+  Menu,
+  Search,
+  LightModeOutlined,
+  DarkModeOutlined,
+  NotificationsNoneOutlined,
+} from "@mui/icons-material";
+import { InputBase, useTheme, Badge } from "@mui/material";
+import { useDispatch } from "react-redux";
+import FlexBetween from "./FlexBetween";
+import { setMode } from "../state";
+
+const Navbar = () => {
+  const dispatch = useDispatch();
+  const theme = useTheme();
+
+  return (
+    <AppBar width="100%" height="100%" position="static">
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        <FlexBetween>
+          <IconButton>
+            <Menu />
+          </IconButton>
+          <FlexBetween
+            backgroundColor={theme.palette.background.alt}
+            gap="3rem"
+            borderRadius="9px"
+            padding="0.1rem 0.7rem"
+          >
+            <InputBase placeholder="Search.." />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        </FlexBetween>
+        <FlexBetween>
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === "light" ? (
+              <DarkModeOutlined />
+            ) : (
+              <LightModeOutlined />
+            )}
+          </IconButton>
+          <IconButton>
+            <Badge color="secondary" variant="dot">
+              <NotificationsNoneOutlined />
+            </Badge>
+          </IconButton>
+        </FlexBetween>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
--- front-end/src/pages/Layout.jsx
+++ front-end/src/pages/Layout.jsx
@@ -1,5 +1,16 @@
+import Box from "@mui/material/Box";
+import Navbar from "../components/Navbar";
+import { Outlet } from "react-router-dom";
+
 const Layout = () => {
-  return <div>Layout</div>;
+  return (
+    <Box>
+      <Box>
+        <Navbar />
+        <Outlet />
+      </Box>
+    </Box>
+  );
 };
 
 export default Layout;
